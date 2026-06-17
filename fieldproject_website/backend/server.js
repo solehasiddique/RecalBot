d0a0435ef94d8315a9624c69d6cea9fee07df8e9
solehasiddique <solehasiddique07@gmail.com>
2026-06-17 22:12:33 +0530

    Base URL change

--- fieldproject_website/backend/server.js
+++ fieldproject_website/backend/server.js
@@ -16,12 +16,12 @@ dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-console.log("ENV CHECK ⛳");
-console.log("EMAIL_USER:", process.env.MAIL_USER);
-console.log("EMAIL_PASS EXISTS:", !!process.env.MAIL_PASS);
-console.log("MongoDB connected ✔");
-console.log("JWT_SECRET EXISTS:", !!process.env.JWT_SECRET);
-console.log("--------------------");
+// console.log("ENV CHECK ⛳");
+// console.log("EMAIL_USER:", process.env.MAIL_USER);
+// console.log("EMAIL_PASS EXISTS:", !!process.env.MAIL_PASS);
+// console.log("MongoDB connected ✔");
+// console.log("JWT_SECRET EXISTS:", !!process.env.JWT_SECRET);
+// console.log("--------------------");
 
 
 const app = express();
@@ -34,17 +34,22 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(",") || [];
 
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin) return callback(null, true);
+
+//     if (allowedOrigins.includes(origin)) {
+//       return callback(null, true);
+//     }
+
+//     return callback(new Error("Not allowed by CORS"));
+//   },
+//   credentials: true,
+// }));
+
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    }
-
-    return callback(new Error("Not allowed by CORS"));
-  },
-  credentials: true,
+  origin: "https://recal-bot.vercel.app",
+  credentials: true
 }));
 
 
--- fieldproject_website/frontend/config.js
+++ fieldproject_website/frontend/config.js
@@ -1,2 +1,1 @@
-const BASE_URL = "https://recalbot-production.up.railway.app";
-window.BASE_URL = BASE_URL;
+window.BASE_URL = "https://recalbot-production.up.railway.app";
